75eb7ee7a13050bab508b61fea08fd3f7e3b474a
Luka_Matešković <luka.mateskovicc@gmail.com>
2025-03-09 17:36:37 +0100

    Dodao sam novi Xcode projekt i SnapKit dependency

---
+++ MyNewApp/MyNewAppTests/MyNewAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyNewAppTests.swift
+//  MyNewAppTests
+//
+//  Created by Luka Matešković on 09.03.2025..
+//
+
+import Testing
+
+struct MyNewAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
