9f3964d1c84131e6ef95d266ea263c96290d72e0
Luka_Matešković <luka.mateskovicc@gmail.com>
2025-03-17 14:02:41 +0100

    Uklonjeni nepotrebni direktoriji i datoteke s hw2 brancha

--- Sofa2025/Sofa2025/Fonts.swift
+++
@@ -1,6 +0,0 @@
-import UIKit
-
-struct Fonts {
-    static let titleFont = UIFont.systemFont(ofSize: 18, weight: .bold)
-    static let subtitleFont = UIFont.systemFont(ofSize: 14, weight: .regular)
-}
--- Sofascore2025/Sofascore2025/Colors.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-struct Colors {
-    static let backgroundColor = UIColor.white
-    static let primaryTextColor = UIColor.black
-    static let secondaryTextColor = UIColor.gray
-}
